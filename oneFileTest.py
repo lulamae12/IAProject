8431b6b7dc35e7f510a11638cb75a599051fab43
Tommy Smith <tls.tommy11@gmail.com>
2019-10-06 09:23:39 -0500

    upload

--- oneFileTest.py
+++ oneFileTest.py
@@ -46,6 +46,7 @@ class floatOrSinkMenu(object):
         #call run class and run flmm section
         runClass("floatMainMenu")
 
+
     def sinkingPressed(self):
         #sinkPressed()
         pass
@@ -65,9 +66,9 @@ class floatMainMenu(object):
         MainWindow.resize(417, 542)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.veiwGraphButton = QtWidgets.QPushButton(self.centralwidget)
-        self.veiwGraphButton.setGeometry(QtCore.QRect(80, 340, 260, 31))
-        self.veiwGraphButton.setObjectName("veiwGraphButton")
+        self.viewGraphButton = QtWidgets.QPushButton(self.centralwidget)
+        self.viewGraphButton.setGeometry(QtCore.QRect(80, 340, 260, 31))
+        self.viewGraphButton.setObjectName("viewGraphButton")
         self.addDataPointButton = QtWidgets.QPushButton(self.centralwidget)
         self.addDataPointButton.setGeometry(QtCore.QRect(80, 300, 260, 31))
         self.addDataPointButton.setObjectName("addDataPointButton")
@@ -112,22 +113,38 @@ class floatMainMenu(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        #Back to float main menu
+
+
+
+        self.quitToSelectButton.clicked.connect(self.backButtonPressed)
+
+    def backButtonPressed(self):
+        MainWindow.close
+        print('ran')
+        runClass("floatOrSinkMenu")
+        print('ran')
+        MainWindow.show
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.veiwGraphButton.setText(_translate("MainWindow", "Veiw Graphs"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Float Data - Main Menu"))
+        self.viewGraphButton.setText(_translate("MainWindow", "view Graphs"))
         self.addDataPointButton.setText(_translate("MainWindow", "Add New Data Point"))
         self.settingsButton.setText(_translate("MainWindow", "Settings"))
         self.quitToSelectButton.setText(_translate("MainWindow", "Quit to Select"))
-        self.editDataPointButton.setText(_translate("MainWindow", "Veiw And Edit Data Points"))
+        self.editDataPointButton.setText(_translate("MainWindow", "view And Edit Data Points"))
         self.addNewDPandGraphButton.setText(_translate("MainWindow", "Add New Data Point and Graph"))
         self.label.setText(_translate("MainWindow", "Floating Data"))
 
 
 def runClass(name):
+    
     MainWindow.close
+    print("1")
     className = eval(name)()
     className.setupUi(MainWindow)
+    print("2")
     MainWindow.show
 
 
